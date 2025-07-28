ba4bd480d197941f1bac6fed6f027b9324656cba
Twoguini <twoguini@gmail.com>
2025-07-28 00:22:32 -0300

    login page done

--- Desafio-Saurus-Software/src/pages/login.tsx
+++ Desafio-Saurus-Software/src/pages/login.tsx
@@ -14,6 +14,10 @@ function Login() {
   const [erro, setErro] = useState<string | null>(null);
 
   useEffect(() => {
+    document.title = "Login";
+  }, [])
+
+  useEffect(() => {
     async function fetchApplications() {
       const res = await GetAplicacoes();
 
@@ -30,19 +34,35 @@ function Login() {
 
   return(
     <div className="login-background">
-      {erro && <p style={{ color: 'red' }}>{erro}</p>}
-      <div>
-        <form className='login-form'>
-          <div className='aplication-select'>
-            <label htmlFor="select_aplication">Aplicação</label>
-            <select name="select_aplication" id="select_aplication">
-              {aplicacoes.map((app) => (
-                <option key={app.id} value={app.id}>{formatReferenceName(app.nomeReferencia)}</option>
-              ))}
-            </select>
-          </div>
-        </form>
-      </div>
+      {erro && <p className='erro-message' >{erro}</p>}
+      <form className='login-form'>
+        
+        <label className='aplication-select'>
+          <label htmlFor="select_aplication" className='application-SelectionLabel'>Aplicação</label>
+          <select name="select_aplication" id="select_aplication">
+            {aplicacoes.map((app) => (
+              <option key={app.id} value={app.id}>{formatReferenceName(app.nomeReferencia)}</option>
+            ))}
+          </select>
+          <label htmlFor="select_aplication" className="select-arrow">▾</label>
+        </label>
+
+        <label className='aplication-select'>
+          <label htmlFor="user-id" className='application-SelectionLabel'>Id Usuário</label>
+          <input type="text" id="user-id" name="user-id" />
+        </label>
+
+        <label className='aplication-select'>
+          <input type="text" id="cpf-cnpj" name="cpf-cnpj" placeholder="CNPJ / CPF" />
+        </label>
+
+        <div className="checkbox-container">
+          <input type="checkbox" id="ambiente" />
+          <label htmlFor="ambiente">Ambiente de Produção</label>
+        </div>
+
+        <button type="submit">Acessar</button>
+      </form>
     </div>
   )
 }
